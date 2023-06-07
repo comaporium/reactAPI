e5d0e2ba0468e5485be5dc999aa6118c0a3dba69
Rijad <rijad.comor@fsk.unsa.ba>
2023-06-07 15:01:53 +0200

    first commit

---
+++ zadaci/Controllers/ZadaciController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using zadaci.Models;
+
+namespace zadaci.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class ZadaciController : Controller
+    {
+        ZadaciContext db = new ZadaciContext();
+
+        [HttpGet]
+        public IActionResult sviZadaci()
+        {
+            List<Zadaci> sviZadaci = db.Zadacis.OrderBy(x => x.Id).ToList();
+            return Ok(sviZadaci);
+        }
+
+        [HttpPost]
+        public IActionResult dodajZadatak(string nazivZadatka)
+        {
+            Zadaci noviZadatak = new Zadaci();
+            noviZadatak.NazivZadatka = nazivZadatka;
+
+            db.Add(noviZadatak);
+            db.SaveChanges();
+
+            return Ok(noviZadatak);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult azurirajZadatak(int id)
+        {
+            Zadaci zadatak = db.Zadacis.Where(x => x.Id == id).FirstOrDefault();   
+            
+            zadatak.Stanje = (byte?)(zadatak.Stanje == 0 ? 1 : 0);
+
+            db.SaveChanges();
+            return Ok(zadatak);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult obrisiZadatak(int id)
+        {
+            Zadaci zadatak = db.Zadacis.Where(x => x.Id == id).FirstOrDefault();
+            if (zadatak == null) return NotFound("Ne postoji zadatak sa tim IDem");
+            db.Remove(zadatak);
+            db.SaveChanges();
+
+            return Ok(zadatak);
+        }
+    }
+}
---
+++ zadaci/Models/ZadaciContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace zadaci.Models;
+
+public partial class ZadaciContext : DbContext
+{
+    public ZadaciContext()
+    {
+    }
+
+    public ZadaciContext(DbContextOptions<ZadaciContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Zadaci> Zadacis { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=RICCI\\SQLEXPRESS;Database=zadaci;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Zadaci>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__zadaci__3213E83F9EB3A634");
+
+            entity.ToTable("zadaci");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.NazivZadatka)
+                .HasMaxLength(200)
+                .HasColumnName("nazivZadatka");
+            entity.Property(e => e.Stanje)
+                .HasDefaultValueSql("((0))")
+                .HasColumnName("stanje");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
